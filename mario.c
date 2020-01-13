a5f11a24fc550608c5d31211ad8a1228a41a2e27
= <=>
2020-01-12 22:06:31 -0600

    Adding blocks. needs fixing.

--- mario.c
+++ mario.c
@@ -4,14 +4,24 @@
 #define MAX_CHARS 25
 
 int main(void){
-    char n[MAX_CHARS];
-    do{
+    char s[MAX_CHARS];
+    int n;
+   
+     do{
         printf("Please enter height: ");
-        fgets(n,MAX_CHARS,stdin);
-        n[slen(n)-1] = '\0';
-    }while(!isnum(n));
+        fgets(s,MAX_CHARS,stdin);
+        s[slen(s)-1] = '\0';
+    }while(!isnum(s));
     
-    
-    printf("%i",slen(n));
+    n = strtoint(s);
+    for(int i = 0; i < n; i++){
+        for(int j = n - 1; j > 0; j--){
+            printf(" ");
+        }
+        for(int k = 1; k < n; k++){
+            printf("#");
+        }
+        printf("\n");
+    }    
     return 0;
 }
